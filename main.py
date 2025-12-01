48c386bd6f5a678307d1e73cb650f8a9b4994b42
GokulCh <gokul.chaluvadi@gmail.com>
2025-11-30 19:19:03 -0500

    feat: Add support for running scaling and baselines experiments via CLI.

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import argparse
 from experiments import exp1_ansatz_comparison
 from experiments import exp2_entanglement_study
 from experiments import exp3_noise_robustness
+from experiments import exp4_scaling
+from experiments import exp5_baselines
 
 def main():
     parser = argparse.ArgumentParser(description="Quantum Autoencoder Project CLI")
-    parser.add_argument('--exp', type=int, choices=[1, 2, 3], help='Experiment number to run (1: Ansatz Comparison, 2: Entanglement Study, 3: Noise Robustness)')
+    parser.add_argument('--exp', type=int, choices=[1, 2, 3, 4, 5], help='Experiment number to run (1: Ansatz Comparison, 2: Entanglement Study, 3: Noise Robustness, 4: Scaling, 5: Baselines)')
     
     args = parser.parse_args()
     
@@ -16,8 +18,12 @@ def main():
         exp2_entanglement_study.run_experiment()
     elif args.exp == 3:
         exp3_noise_robustness.run_experiment()
+    elif args.exp == 4:
+        exp4_scaling.run_experiment()
+    elif args.exp == 5:
+        exp5_baselines.run_experiment()
     else:
-        print("Please specify an experiment number using --exp [1, 2, 3]")
+        print("Please specify an experiment number using --exp [1, 2, 3, 4, 5]")
         parser.print_help()
 
 if __name__ == "__main__":
